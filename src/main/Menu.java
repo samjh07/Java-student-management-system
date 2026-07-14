c30e60550762632e4fe4ec84efc82d9b92046ab1
samjh07 <samridhjha17@gmail.com>
2026-07-14 22:52:17 +0530

    Update Menu.java

---
+++ src/main/Menu.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Menu {
+
+    public static void showMenu(){
+
+        System.out.println("1. Add Student");
+        System.out.println("2. View Students");
+        System.out.println("3. Update Student");
+        System.out.println("4. Delete Student");
+        System.out.println("5. Exit");
+    }
+}
